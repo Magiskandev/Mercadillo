2e56f45180058862e6b8c14fa5571313131bf8b7
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-12-04 02:31:14 -0500

    Aggregated product data

---
+++ src/ProductsData.jsx
@@ -0,0 +1,194 @@
+const ProductsData = [
+    {
+        section: "fas fa-cubes",
+        image: "https://d26lpennugtm8s.cloudfront.net/stores/257/518/products/vasos11-eea4a6215faca1de7715123616989100-640-0.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Decoración",
+        title: "Vasos Desechables",
+        descriptionProduct: "Paquete de vasos x12, con capacidad de 200ml, ideales para las fiestas.",
+        price: "4.900$",
+        txtBtn: "Agregar"
+    },
+    {
+        section: "fas fa-cubes",
+        image: "https://dojiw2m9tvv09.cloudfront.net/21066/product/pulseraangeldelaguarda3305.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Accesorios",
+        title: "Pulsera Ángel de la Guarda",
+        descriptionProduct: "Cada piedra de murano tiene un significado especial, ideal para un regalo",
+        price: "45.700$",
+        txtBtn: "Agregar"
+    },
+    {
+        section: "fas fa-cubes",
+        image: "https://ebani.com.co/wp-content/uploads/2020/01/Fuente-natural-con-dos-bons%C3%A1is.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Ecológico",
+        title: "Fuente Natural con dos Bonsais",
+        descriptionProduct: "Fuente natural con dos Bosais Shimpaku >5 años de edad. Su caída de agua es muy relajante, especialmente para lugares estresantes como la casa u oficina y algo importante, se adaptan muy bien a interiores. Brindándote frescura, confort y un fino aroma a pino y bosque.",
+        price: "310.000$",
+        txtBtn: "Agregar"
+    },
+    {
+        section: "fas fa-cubes",
+        image: "https://comercialpapelera.co/tienda/306-medium_default/bombas-sempertex-r-12-infinity-feliz-cumpleaos-surt-x-12.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Decoración",
+        title: "BOMBAS SEMPERTEX R-12 INFINITY FELIZ CUMPLEAÑOS SURT X 12",
+        descriptionProduct: "Celebra tus fiestas y reuniones con los elementos ms originales, Bomba tamao: R-12 (30 cm) X 12 UND, diseo FELIZ CUMPLEAOS, infinity, colores surtidos Fucsia, Rojo, Amarillo, Verde Lima, Azul Caribe, Azul Rey, Violeta, Naranja.",
+        price: "8,500$",
+        txtBtn: "Agregar"
+    },
+    {
+        section: "fas fa-cubes",
+        image: "https://http2.mlstatic.com/D_NQ_NP_722262-MCO31111289212_062019-O.webp",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Utilidades",
+        title: "Navaja Suiza Original",
+        descriptionProduct: "¿Una excursión de alpinismo o de trekking? Con la Climber llegará más lejos.",
+        price: "139.000$",
+        txtBtn: "Agregar"
+    },
+    {
+        section: "fas fa-hamburger",
+        image: "https://cocina-casera.com/wp-content/uploads/2016/11/hamburguesa-queso-receta.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Comida rapida",
+        title: "Hamburquesa doble queso",
+        descriptionProduct: "Deliciosa hamburquesa con doble queso, carne de 100 libras, tomate, lechuga y cebolla.",
+        price: "15.800$",
+        txtBtn: "Agregar"
+    },
+    {
+        section: "fas fa-hamburger",
+        image: "https://dam.cocinafacil.com.mx/wp-content/uploads/2019/03/pizza-gourmet-01.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Gourmet",
+        title: "Pizza gourmet",
+        descriptionProduct: "Pizza saludable ideal para esas personas que cuidan su linea",
+        price: "22.200$",
+        txtBtn: "Agregar"
+    },
+    {
+        section: "fas fa-hamburger",
+        image: "https://okdiario.com/img/2016/08/15/crepe-de-jamon-y-queso.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Comidas",
+        title: "Crepes de jamón y queso",
+        descriptionProduct: "Las crepes de jamón y queso son un clásico desayuno, merienda o cena rápida que nunca falla.",
+        price: "16.700$",
+        txtBtn: "Agregar"
+    },
+    {
+        section: "fas fa-hamburger",
+        image: "https://jumboargentina.vteximg.com.br/arquivos/ids/593112-750-750/Helado-Sei-T-Palito-Bomb-n-390-Gr-1-36120.jpg?v=637303801182000000",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Helados",
+        title: "Helado Sei Tú Palito Bombón",
+        descriptionProduct: "Helado Sei Tú Palito Bombón 390 Gr",
+        price: "5.850$",
+        txtBtn: "Agregar"
+    },
+    {
+        section: "fas fa-hamburger",
+        image: "https://www.laestacionbistro.com/wp-content/uploads/2018/05/21022009074-768x576.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Charcuteria",
+        title: "TABLA DE QUESOS Y FIAMBRES",
+        descriptionProduct: "Una selección de nuestros quesos y embutidos, ideal para compartir y acompañar con un vino.",
+        price: "48.600$",
+        txtBtn: "Agregar"
+    },
+    {
+        section: "fas fa-business-time",
+        image: "https://www.computronixco.com/wp-content/uploads/2020/02/mantenimiento-de-computadores-cali-reparacion-computadores-06.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Mantenimiento",
+        title: "Reparación de computadores",
+        descriptionProduct: "Se reparan computadores en todo el area metropolitana de Medellín.",
+        price: "?$",
+        txtBtn: "Cotizar"
+    },
+    {
+        section: "fas fa-business-time",
+        image: "https://www.merca2.es/wp-content/uploads/2020/03/coronaviruso-no-pintar-u%C3%B1as-1280x720-1.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Salud y belleza",
+        title: "Servicio de Manicure y Pedicure",
+        descriptionProduct: "Pintado y limpieza de uñas",
+        price: "?$",
+        txtBtn: "Cotizar"
+    },
+    {
+        section: "fas fa-business-time",
+        image: "https://publicarros.com/uploaded_images/CA83936750EN83685244.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Automotores",
+        title: "2010 Volkswagen Bora",
+        descriptionProduct: "Se vende carro usado modelo 2010 en excelentes condiciones.",
+        price: "25.000.000$",
+        txtBtn: "Cotizar"
+    },
+    {
+        section: "fas fa-business-time",
+        image: "https://i.linio.com/p/9c528615868cbeece4bc93213c0eb6a7-product.webp",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Entretenimiento",
+        title: "Uncharted 4 Ps4 Fisico",
+        descriptionProduct: "Nuevo juego Uncharted 4 para Ps4, original.",
+        price: "79.900$",
+        txtBtn: "Cotizar"
+    },
+    {
+        section: "fas fa-business-time",
+        image: "https://www.emedicalbox.com/wp-content/uploads/2019/10/tramitador-procesal-articulo-710x418.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Recados",
+        title: "Tramitador",
+        descriptionProduct: "se realizan tramites de cualquier tipo.",
+        price: "?$",
+        txtBtn: "Cotizar"
+    },
+    {
+        section: "fas fa-store-alt",
+        image: "https://jumbocolombiafood.vteximg.com.br/arquivos/ids/3484423-750-750/7702189054036.jpg?v=637175561956630000",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Abarrotes",
+        title: "De Todito (Natural)",
+        descriptionProduct: "Pasabocas de todito natural x150g",
+        price: "4,390$",
+        txtBtn: "Agregar"
+    },
+    {
+        section: "fas fa-store-alt",
+        image: "https://tiendatellevo.com/wp-content/uploads/2020/07/aguila-litro.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Estanquillo",
+        title: "CERVEZA AGUILA 1000ml",
+        descriptionProduct: "Águila Original, cerveza lager clásica, sabrosa y refrescante que ha sido disfrutada durante más de un siglo.",
+        price: "5.000$",
+        txtBtn: "Agregar"
+    },
+    {
+        section: "fas fa-store-alt",
+        image: "https://www.eurosupermercados.com/eurosupermercado2020//contenidos/images/ecommerce_productos/82136.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Fruver",
+        title: "Sandia",
+        descriptionProduct: "Sandia fresca, ideal para los dias calurosos",
+        price: "1,200$",
+        txtBtn: "Agregar"
+    },
+    {
+        section: "fas fa-store-alt",
+        image: "https://frigocarnes.co/wp-content/uploads/2018/12/FRIGOCARNES_SOLOMO-REDONDO.jpg",
+        tagIcon: "fas fa-tag",
+        tagDescription: "Cárnicos",
+        title: "Solomo",
+        descriptionProduct: "El Solomo o Lomo ancho está ubicado en el cuarto anterior del bovino, en la región dorsal y lumbar, es un corte blando con forma rectangular gruesa.",
+        price: "7.900$",
+        txtBtn: "Agregar"
+    }
+]
+
+export default ProductsData;
