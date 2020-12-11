ffdbc9d92b4fab0cf5f5342571df65db10144ab2
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-12-10 20:27:36 -0500

    Added style modularization. Close #1

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,152 +1,91 @@
 import React from 'react';
-import {Card} from 'react-bootstrap'
-import {Button} from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 
-function CardProduct ({products, loading}){
+
+function CardProduct({ products, loading }) {
 
   if (loading) {
     return <h2>Loading...</h2>;
   }
 
-    return (
-        <>
+  return (
+    <>
       { products.map(product => (
-
-
-        <Card style={{ width: '337px', height: '236px', marginLeft: '24px',
-        marginBottom: '32px'}}
-        className="mt-5" >
-            <div className="">
-{/* //////////////////////Header Card////////////////////// */}
-                  <div
-                  className="d-flex flex-row
+        <Card
+          className="mt-5 card-product">
+          {/* //////////////////////Header Card////////////////////// */}
+          <div
+            className="d-flex flex-row
                   position-absolute
                   justify-content-between
                   px-3
                   align-items-center
-                  justify-content-center"
-                  style={{
-                    width: '337px',
-                    height: '50px'}}>
-                      <span
-                  style={{
-                    width: '35px',
-                    height: '35px',
-                    background: '#C1C1C1',
-                    borderRadius: '50%',
-                    }}
-                    className="d-flex
+                  justify-content-center card-header">
+            <span
+              className="d-flex
                     justify-content-center
-                    align-items-center">
-                <i className={product.companyLogo}
-                style={{
-                  color: '#343232',
-                  fontSize: '20px'
-                  }}>
+                    align-items-center card-header-span">
+              <i className={`${product.companyLogo} card-company-logo`}></i>
+            </span>
+            <h3 className="card-company-name">{product.companyName}</h3>
+          </div>
+          <div className="card-color-head"></div>
+          {/* //////////////////////Header Card////////////////////// */}
+
+
+          {/* //////////////////////body Card////////////////////// */}
+          <section className="d-flex "
+            style={{ background: product.backgroundBody }}>
+            <Card.Img variant="top" src={product.image} className="card-image" />
+
+            <div>
+              <div
+                className="position-absolute pr-2 d-flex justify-content-end
+    align-items-center card-content-icon">
+                <i className={`${product.tagIcon} card-icon-tag`}></i>
+              </div>
+              <div className="position-absolute d-flex justify-content-center
+    align-items-center card-content-category">
+                <i className={`${product.section} card-color-icon-category`}></i>
+              </div>
+            </div>
+
+
+            <section className="d-flex flex-column">
+              <Card.Body className="card-body-content">
+                <Card.Text className="card-body-text">
+                  {product.descriptionProduct}
+                </Card.Text>
+              </Card.Body>
+              <div className="d-flex justify-content-center align-items-center
+    justify-content-between card-content-btns">
+                <i
+                  className={`${product.likeIcon} card-style-icon-like d-flex
+          justify-content-center align-items-center`}>
                 </i>
-                  </span>
-                  <h3 style={{
-                    fontFamily: 'Lato, sans-serif',
-                    fontStyle: 'normal',
-                    fontWeight: 'bold',
-                    fontSize: '16px',
-                    color: '#EEECED',
-                    }}>{product.companyName}</h3>
-                  </div>
-             
-            <div
-            style={{
-                borderTopLeftRadius: '10px',
-                borderTopRightRadius: '10px',
-                width: '337px',
-                height: '50px',
-                background: '#343232' }}
-                >
+                <Button variant="primary" className="card-btn-add"
+                  style={{ background: product.backgroundButton }}>
+                  {product.txtBtn}</Button>
+              </div>
+            </section>
+            {/* //////////////////////Body////////////////////// */}
+
+            {/* //////////////////////Footer////////////////////// */}
+            <div className="justify-content-center position-absolute d-flex
+  justify-content-between px-3 align-items-center card-content-footer"
+              style={{ background: product.backgroundFooter }}>
+              <Card.Title className="card-title-footer">
+                {product.title}</Card.Title>
+              <Card.Title className="card-title-two-footer">
+                {product.price}</Card.Title>
             </div>
-{/* //////////////////////Header Card////////////////////// */}
-
-{/* //////////////////////body Card////////////////////// */}
-
-{/* //////////////////////Image////////////////////// */}
-            </div>
-            <section className="d-flex "
-            style={{background: product.backgroundBody}}>
-  <Card.Img variant="top" src={product.image}
-  style={{ width: '135px', height: '145px',
-  borderTopRightRadius:'24px', zIndex: '2'}}/>
-  {/* //////////////////////Image////////////////////// */}
-
-{/* //////////////////////Icons////////////////////// */}
-  <div>
-    <div 
-    className="position-absolute pr-2 d-flex justify-content-end
-    align-items-center"
-    style={{ width: '54px', height: '30px', left: '115px',
-  borderBottomRightRadius:'10px', background: '#FFFFFF', top:'50px'}}>
-      <i className={product.tagIcon}
-      style={{ textAlign: 'right'}}></i>
-    </div>
-    <div className="position-absolute d-flex justify-content-center
-    align-items-center"
-    style={{ background: '#9E0059', width: '40px', height: '30px',
-    right:0,  borderBottomLeftRadius: '10px'}}>
-      <i className={product.section}
-      style={{ color: '#EEECED'}}></i>
-      </div>
-      </div>
-{/* //////////////////////Icons////////////////////// */}
-
-{/* //////////////////////Body////////////////////// */}
-<section className="d-flex flex-column">
-  <Card.Body
-  style={{padding: '32px 16px 0', height: '80px'}}>
-    <Card.Text style={{ fontSize: '12px', LineHeight: '14px',
-  fontWeight: 'normal', fontFamily: 'Roboto, sans-serif'}}>
-      {product.descriptionProduct}
-    </Card.Text>
-    </Card.Body>
-    <div className="d-flex justify-content-center align-items-center
-    justify-content-between"
-    style={{height: '60px', padding: '0 0 0 16px'}}>
-            <i style={{ background: '#FFFFFF', height: '40px', width: '40px',
-          borderRadius: '50%', verticalAlign: 'middle',
-        display: 'flex', justifyContent: 'center', alignItems: 'center'}}
-          className={product.likeIcon}>
-      </i>
-    <Button variant="primary" className=""
-    style={{ background: product.backgroundButton, height: '40px', border: 'none',
-    width: '125px', borderTopRightRadius: '0', borderBottomRightRadius: '0'}}>
-      {product.txtBtn}</Button>
-      </div>
-      </section>
-  {/* //////////////////////Body////////////////////// */}
-
-  {/* //////////////////////Footer////////////////////// */}
-  <div className="justify-content-center position-absolute d-flex
-  justify-content-between px-3 align-items-center"
-  style={{ width: '100%', height: '40px',
-  background: product.backgroundFooter, bottom: '0', borderBottomLeftRadius: '10px',
-  borderBottomRightRadius: '10px'}}>
-  <Card.Title
-  style={{ color: '#EEECED',
-  fontFamily: 'Roboto, sans-serif',
-fontWeight: 'bold',
-fontSize: '14px'}}>
-    {product.title}</Card.Title>
-  <Card.Title
-  style={{ color: '#EEECED',
-  fontFamily: 'Roboto, sans-serif',
-fontWeight: 'bold',
-fontSize: '14px'}}>
-    {product.price}</Card.Title>
-  </div>
-{/* //////////////////////Footer////////////////////// */}
-  </section>
-</Card>
-
-))}
-
-</>
-)}
+            {/* //////////////////////Footer////////////////////// */}
+          </section>
+        </Card>
+      ))}
+    </>
+  )
+}
 
 export default CardProduct
