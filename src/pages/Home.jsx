923f4b7c93ee58321965647921a96bc291b7bf3f
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-12-27 12:30:48 -0500

    home section moved to products section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import CardProducts from '../components/home/CardProducts'
-import CarouselApp from '../components/home/CarouselApp';
-import SearchBar from '../components/home/SearchBar';
+
 
 function Home() {
   return (
     <section className="global-section-home">
-      <CarouselApp />
-      <SearchBar />
-      <CardProducts />
+
     </section>
   );
 }
--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,10 +1,15 @@
-import {Component} from 'react';
+import { Component } from 'react';
+import CardProducts from '../components/home/CardProducts'
+import CarouselApp from '../components/home/CarouselApp';
+import SearchBar from '../components/home/SearchBar';
 
-export default class Productos extends Component{
-    render(){
-        return(
-            <section>
-                Productos
+export default class Productos extends Component {
+    render() {
+        return (
+            <section className="global-section-home">
+                <CarouselApp />
+                <SearchBar />
+                <CardProducts />
             </section>
         )
     }
