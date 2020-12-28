89d9fea8d0275087467f7d0806883b990c87ed3e
Santiago Betancur Graciano <santybg_94@hotmail.com>
2020-12-27 21:22:46 -0500

    pull dec 27

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,10 +1,13 @@
 import {Component} from 'react';
+import CarouselApp from '../components/productos/CarouselApp';
+import SearchBar from '../components/productos/SearchBar';
 
 export default class Productos extends Component{
     render(){
         return(
             <section>
-                Productos
+                {/* <CarouselApp/> */}
+                <SearchBar/>
             </section>
         )
     }
