ce05fc0c85856d37beeff75db287bf0e005b7534
Santiago Betancur Graciano <santybg_94@hotmail.com>
2020-12-29 12:05:45 -0500

    Se trata de mover el carousel a productos

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,12 +1,12 @@
 import {Component} from 'react';
-import CarouselApp from '../components/productos/CarouselApp';
+import CarouselApp from '../components/home/CarouselApp';
 import SearchBar from '../components/productos/SearchBar';
 
 export default class Productos extends Component{
     render(){
         return(
             <section>
-                {/* <CarouselApp/> */}
+                <CarouselApp/>
                 <SearchBar/>
             </section>
         )
