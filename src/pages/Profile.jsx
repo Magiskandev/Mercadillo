b4127bb3fbac81f05381117dbb0bfcacf3767a61
Santiago Betancur Graciano <santybg_94@hotmail.com>
2020-12-29 14:01:14 -0500

    resolving confÃlicts

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,9 @@
 import { Component } from 'react';
 import ProductRegister from '../components/perfil/ProductRegister'
 
-<<<<<<< HEAD
-export default class Perfil extends Component{
-    render(){
-        return(
-=======
-export default class Perfil extends Component {
+export default class Profile extends Component {
     render() {
         return (
->>>>>>> master
             <section className="global-section-home">
                 <div className='ml-5'>
                     <ProductRegister />
