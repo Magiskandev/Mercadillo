d7fae8b501a2d4f72c76c351d40e7976692ac877
Santiago Betancur Graciano <santybg_94@hotmail.com>
2021-01-05 17:27:28 -0500

    Trying to fixed the carousel so it renders 3 cards in one item Jan 5th

--- src/components/products/CarouselApp.jsx
+++ src/components/products/CarouselApp.jsx
@@ -3,7 +3,7 @@ import { Carousel, Card, CardDeck, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class CarouselApp extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       products: [{}],
@@ -11,41 +11,41 @@ export default class CarouselApp extends React.Component {
     }
   }
 
-  loadPromotions=()=>{
+  loadPromotions = () => {
     fetch('https://pruebafiltro.tiagobg.repl.co/products')
-    .then((response)=>{
+      .then((response) => {
         return response.json()
-    })
-    .then((data)=>{
+      })
+      .then((data) => {
         this.setState({
-            products: data.sort(function(a, b){
-              return b.discount - a.discount;
+          products: data.sort(function (a, b) {
+            return b.discount - a.discount;
           }),
           title: 'Promociones'
         });
         console.table(this.state.products)
-    })
-    .catch((error)=>{
+      })
+      .catch((error) => {
         console.log(error)
-    })
+      })
   }
-  loadPopulars=()=>{
+  loadPopulars = () => {
     fetch('https://pruebafiltro.tiagobg.repl.co/products')
-    .then((response)=>{
+      .then((response) => {
         return response.json()
-    })
-    .then((data)=>{
+      })
+      .then((data) => {
         this.setState({
-            products: data.sort(function(a, b){
-              return b.timeSearched - a.timeSearched;
+          products: data.sort(function (a, b) {
+            return b.timeSearched - a.timeSearched;
           }),
           title: 'Los más buscados'
         });
         console.table(this.state.products)
-    })
-    .catch((error)=>{
+      })
+      .catch((error) => {
         console.log(error)
-    })
+      })
   }
   render() {
     return (
@@ -54,25 +54,51 @@ export default class CarouselApp extends React.Component {
         <button className='btn btn-warning mx-2' onClick={this.loadPopulars}>Los más buscados</button>
         <h2 className='my-3'>{this.state.title}</h2>
         <Carousel>
-           <Carousel.Item>
-                <CardDeck>
-                  {this.state.products.slice(0,6).map((item, i) =>{
-                    return<Card key={i}>
-                      <Card.Img variant="top" src="holder.js/100px160" />
-                      <Card.Body>
-                        <Card.Title>{item.name}</Card.Title>
-                        <Card.Text>
-                        {item.description}
-                        </Card.Text>
-                      </Card.Body>
-                      <Button variant="primary">Buy</Button>
-                      <Card.Footer>
-                        <small className="text-muted">{item.company}</small>
-                      </Card.Footer>
-                    </Card>    
-                  })}
-                </CardDeck> 
-              </Carousel.Item>
+          {this.state.products.slice(0, 9).map((item, i) => {
+            return <Carousel.Item>
+              <CardDeck>
+                <Card key={i}>
+                  <Card.Img variant="top" src="holder.js/100px160" />
+                  <Card.Body>
+                    <Card.Title>{item.name}</Card.Title>
+                    <Card.Text>
+                      {item.description}
+                    </Card.Text>
+                  </Card.Body>
+                  <Button variant="primary">Buy</Button>
+                  <Card.Footer>
+                    <small className="text-muted">{item.company}</small>
+                  </Card.Footer>
+                </Card>
+                <Card key={i + 1}>
+                  <Card.Img variant="top" src="holder.js/100px160" />
+                  <Card.Body>
+                    <Card.Title>{item.name}</Card.Title>
+                    <Card.Text>
+                      {item.description}
+                    </Card.Text>
+                  </Card.Body>
+                  <Button variant="primary">Buy</Button>
+                  <Card.Footer>
+                    <small className="text-muted">{item.company}</small>
+                  </Card.Footer>
+                </Card>
+                <Card key={i + 2}>
+                  <Card.Img variant="top" src="holder.js/100px160" />
+                  <Card.Body>
+                    <Card.Title>{item.name}</Card.Title>
+                    <Card.Text>
+                      {item.description}
+                    </Card.Text>
+                  </Card.Body>
+                  <Button variant="primary">Buy</Button>
+                  <Card.Footer>
+                    <small className="text-muted">{item.company}</small>
+                  </Card.Footer>
+                </Card>
+              </CardDeck>
+            </Carousel.Item>
+          })}
         </Carousel>
       </section>
     );
