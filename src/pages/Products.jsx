5aba2b05c6469a747e3d7ea04469234f8de43c47
David Montoya Ramírez <deividelpaisa@hotmail.com>
2021-01-06 03:22:14 -0500

    component card product information added

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,17 +2,20 @@ import { Component } from 'react';
 import CardProducts from '../components/products/CardProducts'
 import CarouselApp from '../components/products/CarouselApp';
 import FilterProducts from '../components/products/FilterProducts';
+import CardInfoSection from '../components/products/CardInfoSection';
 import NavPrincipal from '../components/base/NavPrincipal';
 
 export default class Products extends Component {
     render() {
         return (
             <section className="global-section-home">
-                <div style={{ backgroundColor: '#ffeaa7', textAlign: 'center' }}>
-                    <h1>Productos</h1>
-                </div>
+                <NavPrincipal />
                 <CarouselApp />
-                <FilterProducts />
+                <div className="d-flex justify-content-between">
+                    <FilterProducts />
+                    <CardInfoSection />
+                </div>
+
                 <CardProducts />
             </section>
         )
