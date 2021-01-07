a280f0612ba108db6778c108e8ef777cf2cf77f2
Santiago Betancur Graciano <santybg_94@hotmail.com>
2021-01-06 22:34:00 -0500

    Trying to add the suggestions to the ppal searchBar

--- src/components/base/SearchPrincipal.jsx
+++ src/components/base/SearchPrincipal.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 function SearchPrincipal(props) {
     return (
-        <div className="d-flex search-products-nav justify-content-center align-items-center px-3" style={{ width: props.WidthSearch }}>
-            <input className="position-relative input-search-products-nav" type="text" placeholder="¿Que producto buscas?" />
-
-            <i class="fas fa-search icon-search-principal-nav position-relative"></i>
-        </div>
+        <section>
+            <div className="d-flex search-products-nav justify-content-center align-items-center px-3" style={{ width: props.WidthSearch }}>
+                <input className="position-relative input-search-products-nav" type="text" placeholder="¿Que producto buscas?" />
+                <i class="fas fa-search icon-search-principal-nav position-relative"></i>
+            </div>
+            <div id="sugerencias">
+            </div>
+        </section>
+        
     )
 }
 
